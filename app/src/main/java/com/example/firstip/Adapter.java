e95035641e452bef6f91de25eeb51e93b4c64dc7
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-23 01:21:03 +0300

    Implemented overriden methods due to super() functionality

--- app/src/main/java/com/example/firstip/Adapter.java
+++ app/src/main/java/com/example/firstip/Adapter.java
@@ -3,18 +3,32 @@ package com.example.firstip;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import androidx.annotation.Nullable;
+
 public class Adapter extends ArrayAdapter {
     private Context mContext;
     private String[] mTeams;
     private String[] mPlayers;
     //resource here is our layout
     public Adapter(Context mContext,int resource,String[] mTeams,String[] mPlayers){
-            //as subclasss,access to overriden methods and private properties
+            //as subClass,access to overriden methods and private properties
         super(mContext,resource);
         this.mContext =mContext;
         this.mPlayers =mPlayers;
         this.mTeams =mTeams;
     }
 
+    @Override
+    public int getCount() {
+        return mTeams.length;
+    }
 
+    @Nullable
+    @Override
+    public Object getItem(int position) {
+        String singleTeam = mTeams[position];
+        String players = mPlayers[position];
+        //format specifiers for strings %s
+        return String.format("%s has % players", singleTeam,players);
+    }
 }
