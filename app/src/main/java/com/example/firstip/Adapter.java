c8a775127c909825dbe74de5479d6c2f48f79c50
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-23 01:11:06 +0300

    Declared constructor for our custom adapter

--- app/src/main/java/com/example/firstip/Adapter.java
+++ app/src/main/java/com/example/firstip/Adapter.java
@@ -1,7 +1,20 @@
 package com.example.firstip;
 
+import android.content.Context;
 import android.widget.ArrayAdapter;
 
 public class Adapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mTeams;
+    private String[] mPlayers;
+    //resource here is our layout
+    public Adapter(Context mContext,int resource,String[] mTeams,String[] mPlayers){
+            //as subclasss,access to overriden methods and private properties
+        super(mContext,resource);
+        this.mContext =mContext;
+        this.mPlayers =mPlayers;
+        this.mTeams =mTeams;
+    }
+
 
 }
