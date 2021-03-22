575e7e3dec5ab276dfffec1549cf814150520313
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 14:48:33 +0300

    Added an event listener and test toast for the team button

--- app/src/main/java/com/example/firstip/MainActivity.java
+++ app/src/main/java/com/example/firstip/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.firstip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTeamBut;
@@ -12,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTeamBut = (Button) findViewById(R.id.findTeamButton);
-
+        mTeamBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //test whether a toast will appear on submit
+                Toast.makeText(MainActivity.this, "Retrieving team detail", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
