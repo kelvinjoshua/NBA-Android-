4832b904203c99d546482cf9476dbef64df178ec
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 17:18:59 +0300

    Implemented butterknife in team activity

--- app/src/main/java/com/example/firstip/Teams.java
+++ app/src/main/java/com/example/firstip/Teams.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Teams extends AppCompatActivity {
+    @BindView(R.id.intentTextView)
     private String Retrieved;
     private TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
-        showText = (TextView) findViewById(R.id.intentTextView);
+        //showText = (TextView) findViewById(R.id.intentTextView);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         Retrieved = intent.getStringExtra("Team");
         //we get text and set it to the display value//
