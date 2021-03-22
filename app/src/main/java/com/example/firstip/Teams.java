8e847e00df58c70c246ab11e5aa17d56c0fa0b89
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 15:35:07 +0300

    Set text retrieved from intent to be displayed

--- app/src/main/java/com/example/firstip/Teams.java
+++ app/src/main/java/com/example/firstip/Teams.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Teams extends AppCompatActivity {
     private String Retrieved;
+    private TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
         Intent intent = getIntent();
         Retrieved = intent.getStringExtra("Team");
+        //we get text and set it to the display value//
+        showText.setText("Your team is : " + Retrieved);
     }
 }
