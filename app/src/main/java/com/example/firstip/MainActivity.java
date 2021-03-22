9a51aa3b178f173b2a2cd6535423ad4e8056f8f4
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 14:58:12 +0300

    Added a member variable to retrieve and store EditText view

--- app/src/main/java/com/example/firstip/MainActivity.java
+++ app/src/main/java/com/example/firstip/MainActivity.java
@@ -6,15 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTeamBut;
+    private EditText mTeamEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTeamBut = (Button) findViewById(R.id.findTeamButton);
+        mTeamEdit = (EditText) findViewById(R.id.editTextTextPersonName);
         mTeamBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
