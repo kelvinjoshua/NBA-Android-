ea3daff9f522dd6d85095bdfc95ab770785b4cbd
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 14:42:50 +0300

    Refactored id names for team button

--- app/src/main/java/com/example/firstip/MainActivity.java
+++ app/src/main/java/com/example/firstip/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.firstip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mTeamBut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTeamBut = (Button) findViewById()
     }
 }
