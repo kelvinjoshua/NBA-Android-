7b2b684c6b8d1977ba075d978c0b608893497805
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-22 17:16:16 +0300

    Implemented butterknife in main activity

--- app/src/main/java/com/example/firstip/MainActivity.java
+++ app/src/main/java/com/example/firstip/MainActivity.java
@@ -9,15 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findTeamButton)
     private Button mTeamBut;
+    @BindView(R.id.editTextTextPersonName)
     private EditText mTeamEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTeamBut = (Button) findViewById(R.id.findTeamButton);
-        mTeamEdit = (EditText) findViewById(R.id.editTextTextPersonName);
+       // mTeamBut = (Button) findViewById(R.id.findTeamButton);
+      //  mTeamEdit = (EditText) findViewById(R.id.editTextTextPersonName);
+        ButterKnife.bind(this);
         mTeamBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
