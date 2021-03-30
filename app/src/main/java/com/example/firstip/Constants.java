d9c9c26c6274a93bbe58edd42a789eea50eb7256
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-30 14:31:23 +0300

    Declared key value pair in Buil.gradle(App) to connect our credentials with Buildconfig

--- app/src/main/java/com/example/firstip/Constants.java
+++ app/src/main/java/com/example/firstip/Constants.java
@@ -2,5 +2,5 @@ package com.example.firstip;
 
 public class Constants {
     public static final String RAPID_BASE_URL = "https://api-nba-v1.p.rapidapi.com/";
-    public static final String RAPID_NBA_KEY = "";
+    public static final String RAPID_NBA_KEY = BuildConfig.RAPID_NBA_KEY;
 }
