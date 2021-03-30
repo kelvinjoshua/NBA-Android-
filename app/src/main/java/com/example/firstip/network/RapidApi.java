ca638233fb3c9f957642bd054ac3c7195bc1e13d
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-30 15:32:13 +0300

    Declared call access object and query parameters for the Interface

--- app/src/main/java/com/example/firstip/network/RapidApi.java
+++ app/src/main/java/com/example/firstip/network/RapidApi.java
@@ -1,11 +1,17 @@
 package com.example.firstip.network;
 
+import com.example.firstip.models.NbaSearchResponse;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RapidApi {
     //relative urls
-   // @GET(teams/confName)
-    //@Query("confname") String East;
-    //@Query("confname") String West;
+    @GET("teams/confName")
+    //call object to perfom http request
+    Call<NbaSearchResponse> getTeams(
+            @Query("confname") String East,
+            @Query("confname") String West);
+
 }
