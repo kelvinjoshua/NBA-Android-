c41c85d6e15381463f7845dc3d68e15c4f51c3d1
kelvinjoshua <kelvinbale78@gmail.com>
2021-04-06 14:44:55 +0300

    Added Createaccount and login activities with corresponding layouts.

---
+++ app/src/main/java/com/example/firstip/ui/CreateAccount.java
@@ -0,0 +1,16 @@
+package com.example.firstip.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstip.R;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
