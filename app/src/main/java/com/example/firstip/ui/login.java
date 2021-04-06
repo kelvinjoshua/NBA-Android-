fdf4754e0f494b6b06b257d9ac99c1177be52617
kelvinjoshua <kelvinbale78@gmail.com>
2021-04-07 01:42:55 +0300

    Added intent logic to start create Account activity when user clicks respective text

--- app/src/main/java/com/example/firstip/ui/login.java
+++ app/src/main/java/com/example/firstip/ui/login.java
@@ -2,15 +2,34 @@ package com.example.firstip.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.firstip.R;
 
-public class login extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class login extends AppCompatActivity  implements View.OnClickListener{
+
+    @BindView(R.id.registerTextView) TextView registerHere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        registerHere.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == registerHere){
+            //Intent to start create account activity
+            Intent intent = new Intent(this,CreateAccount.class);
+            startActivity(intent);
+            finish();//destroy this activity due to stack to prevent unnecessary back visits
+        }
     }
 }
