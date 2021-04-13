21a4d8ba01a704b7a28b6dafa578042a267efb76
kelvinjoshua <kelvinbale78@gmail.com>
2021-04-13 11:44:43 +0300

    Added logic to interfaces

--- app/src/main/java/com/example/firstip/util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/example/firstip/util/ItemTouchHelperAdapter.java
@@ -1,4 +1,7 @@
 package com.example.firstip.util;
 
 public interface ItemTouchHelperAdapter {
+    //move ,swipe events
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
 }
--- app/src/main/java/com/example/firstip/util/OnStartDragListener.java
+++ app/src/main/java/com/example/firstip/util/OnStartDragListener.java
@@ -1,4 +1,8 @@
 package com.example.firstip.util;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 public interface OnStartDragListener {
+    //when user interacts,drag and drop action,viewHolder is obj being moved
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
